b6423c32891d00e406e6a6721b0e234bc3706938
Fournier-Paul <paul.fournier.3314@gmail.com>
2025-06-06 19:34:04 +0200

    add components and menus

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,75 @@
+'use client'
+
+import { useState } from 'react'
 import Header from "@/components/Header"
+import MobileNav from "@/components/sub-components/MobileNav"
 import Hero from "@/components/Hero"
-import Footer from "@/components/Footer"
 import Projects from "@/components/Projects"
+import Footer from "@/components/Footer"
 import SkillsCanvas from '@/components/SkillsCanvas'
-import SkillCategoryCards from "@/components/SkillCategoryCards";
-
-
+import SkillCategoryCards from "@/components/SkillCategoryCards"
+import Education from "@/components/Education"
+import Contact from "@/components/Contact"
+import Timeline from "@/components/Timeline"
+import { AnimatePresence, motion } from "framer-motion"
+import type { Section } from '@/components/sub-components/constants'
 
 export default function Home() {
+  const [currentSection, setCurrentSection] = useState<Section>('home')
+
+  const renderSection = () => {
+    switch (currentSection) {
+      case 'home':
+        return <Hero />
+      case 'skills':
+        return (
+          <>
+            <SkillsCanvas />
+            <SkillCategoryCards />
+          </>
+        )
+      case 'projects':
+        return <Projects />
+      case 'experience':
+        return <Timeline />
+      case 'education':
+        return <Education />
+      case 'contact':
+        return <Contact />
+      default:
+        return <Hero />
+    }
+  }
+
   return (
-    <>
-      <Header />
-      <main>
-        <Hero />
-         <Projects />
-         <SkillsCanvas />
-         <SkillCategoryCards />
+<>
+  <Header
+    onSectionChange={setCurrentSection}
+    currentSection={currentSection}
+  />
+
+  <MobileNav
+    onSectionChange={setCurrentSection}
+    currentSection={currentSection}
+  />
+
+  <MobileNav
+    onSectionChange={setCurrentSection}
+    currentSection={currentSection}
+  />
+
+      <main className="min-h-[92vh]">
+        <AnimatePresence mode="wait">
+          <motion.div
+            key={currentSection}
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -20 }}
+            transition={{ duration: 0.4 }}
+          >
+            {renderSection()}
+          </motion.div>
+        </AnimatePresence>
       </main>
       <Footer />
     </>
