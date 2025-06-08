3895d276761b58e208934a32dc62ccf6deb4417a
Fournier-Paul <paul.fournier.3314@gmail.com>
2025-06-08 18:37:48 +0200

    switch defaultTheme to system

--- src/components/sub-components/ThemeProvider.tsx
+++ src/components/sub-components/ThemeProvider.tsx
@@ -12,7 +12,7 @@ export function ThemeProvider({ children }: { children: ReactNode }) {
   }, []);
 
   return (
-    <NextThemesProvider attribute="class" defaultTheme="dark" enableSystem>
+    <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
       {mounted ? children : null}
     </NextThemesProvider>
   );
