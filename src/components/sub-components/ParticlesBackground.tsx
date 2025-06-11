8e31fe16a059c4bb8876eadda503f871dec6ecf0
Fournier-Paul <paul.fournier.3314@gmail.com>
2025-06-11 12:40:56 +0200

    tests fix display on google pixel

--- src/components/sub-components/ParticlesBackground.tsx
+++ src/components/sub-components/ParticlesBackground.tsx
@@ -24,7 +24,7 @@ export default function ParticlesBackground() {
   if (!mounted) return null;
 
   return (
-    <div className="pointer-events-none fixed top-0 left-0 w-full h-full z-0">
+    <div className="pointer-events-none fixed top-0 left-0 w-full h-full z-10">
       <Particles
         id="tsparticles"
         init={particlesInit}
