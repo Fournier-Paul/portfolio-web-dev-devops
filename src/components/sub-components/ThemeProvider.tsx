368f9652e7480989c8956fda2a1f150235c4ae6c
Fournier-Paul <paul.fournier.3314@gmail.com>
2025-06-11 12:27:00 +0200

    ignore theme user system

--- src/components/sub-components/ThemeProvider.tsx
+++ src/components/sub-components/ThemeProvider.tsx
@@ -1,18 +1,14 @@
-'use client';
-
-import { ThemeProvider as NextThemesProvider } from 'next-themes';
-import { ReactNode, useEffect, useState } from 'react';
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { ReactNode } from "react";
 
 export function ThemeProvider({ children }: { children: ReactNode }) {
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
   return (
-    <NextThemesProvider attribute="class" defaultTheme="dark" enableSystem>
-      {mounted ? children : null}
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="dark"
+      enableSystem={false}
+    >
+      {children}
     </NextThemesProvider>
   );
 }
